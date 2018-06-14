feee1a0a6450e4578595ec90a709a00be645a1bc
huna79 <hubert.nachbaur@gmail.com>
2018-06-14 13:35:38 +0200

    new sql scripts for fast recovery area size and used size

---
+++ zabora/sql/db_frasize.sql
@@ -0,0 +1,22 @@
+SET      pagesize 0
+SET      heading OFF
+SET      feedback OFF
+SET      verify OFF
+SELECT
+    CASE
+        WHEN cant = TO_CHAR(0) THEN TO_CHAR(0)
+        ELSE (
+            SELECT
+                TO_CHAR(space_limit,'FM99999999999999990')
+            FROM
+                v$recovery_file_dest
+        )
+    END
+FROM
+    (
+        SELECT
+            TO_CHAR(COUNT(*) ) cant
+        FROM
+            v$recovery_file_dest
+    );
+QUIT;
