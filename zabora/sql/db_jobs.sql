acb46ccc029937111d333e80893810c90297dbcc
huna79 <hubert.nachbaur@gmail.com>
2018-06-14 12:41:05 +0200

    sql statement for broken jobs check

--- zabora/sql/db_jobs.sql
+++ zabora/sql/db_jobs.sql
@@ -2,5 +2,10 @@ SET      pagesize 0
 SET      heading OFF
 SET      feedback OFF
 SET	 verify OFF
-...
+SELECT
+    TO_CHAR(COUNT(*),'FM99999999999999990') retvalue
+FROM
+    dba_jobs
+WHERE
+    broken = 'Y';
 QUIT;
