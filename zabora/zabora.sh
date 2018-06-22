ac9aa32abdab0a8bf74a08dfde8b14f46ed20c9d
huna79 <hubert.nachbaur@gmail.com>
2018-06-22 15:56:27 +0200

    revert last commit

--- zabora/zabora.sh
+++ zabora/zabora.sh
@@ -99,7 +99,7 @@ done
 
 if [[ -f "${SQL%.sql}.sql" ]]; then
     load_oracle
-    rval=$(sqlplus -S -L ${ORACLE_USER}/${ORACLE_PASS} @${SQL} "${SQL_ARGS}")
+    rval=$(sqlplus -S -L ${ORACLE_USER}/${ORACLE_PASS} @${SQL} "${SQL_ARGS}") || { echo "ZBX_NOTSUPPORTED"; exit 1; }
     rcode="${?}"
     if [[ ${JSON} -eq 1 ]]; then
        set -A rval ${rval}
