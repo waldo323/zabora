a9d5fcf1d2d0c10be474a091eb083db7609ec522
huna79 <hubert.nachbaur@gmail.com>
2018-08-23 11:45:02 +0200

    bump zabora version

--- zabora/zabora.sh
+++ zabora/zabora.sh
@@ -11,7 +11,7 @@ PATH=/usr/local/bin:${PATH}
 #
 APP_NAME=$(basename $0)
 APP_DIR=$(dirname $0)
-APP_VER="1.5.3.2"
+APP_VER="1.5.3.3"
 APP_WEB="https://github.com/huna79/zabora"
 #
 #################################################################################
