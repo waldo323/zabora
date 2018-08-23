c1dd27c6e7aef6ee185fface801d37e039888e9f
huna79 <hubert.nachbaur@gmail.com>
2018-08-23 14:40:04 +0200

    use correct timezone

--- zabora/sql/db_rman.sql
+++ zabora/sql/db_rman.sql
@@ -4,7 +4,7 @@ SET      feedback OFF
 SET	 verify OFF
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 SELECT
-    TO_CHAR(trunc(MAX( (end_time) - TO_DATE('01-JAN-1970','DD-MON-YYYY') ) * (86400) ),'FM99999999999999990') retvalue
+    TO_CHAR(trunc( (CAST( (from_tz(CAST(MAX(end_time) AS TIMESTAMP),sessiontimezone) AT TIME ZONE 'GMT') AS DATE) - TO_DATE('01.01.1970','DD.MM.YYYY') ) * 86400),'FM99999999999999990') retvalue
 FROM
     v$rman_backup_job_details
 WHERE
