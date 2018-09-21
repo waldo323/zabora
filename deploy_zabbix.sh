6b21fd771644f77914733b274a9e3aba19abb8c9
d9394 <9394952@gmail.com>
2018-09-21 14:16:56 +0800

    Update deploy_zabbix.sh

--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -6,10 +6,11 @@ ORACLE_USER=${1:-monitor}
 ORACLE_PASS=${2:-xxxxxxx}
 
 mkdir -p ${ZABBIX_DIR}/scripts/agentd/zabora
-cp -rpv  ${SOURCE_DIR}/zabora/sql                 ${ZABBIX_DIR}/scripts/agentd/zabora/
-cp -rpv  ${SOURCE_DIR}/zabora/zabora.conf.example ${ZABBIX_DIR}/scripts/agentd/zabora/zabora.conf
-cp -rpv  ${SOURCE_DIR}/zabora/zabora.sh           ${ZABBIX_DIR}/scripts/agentd/zabora/
-cp -rpv  ${SOURCE_DIR}/zabora/zabbix_agentd.conf  ${ZABBIX_DIR}/zabbix_agentd.d/zabora.conf
+cp -rpv  ${SOURCE_DIR}/zabora/sql                   ${ZABBIX_DIR}/scripts/agentd/zabora/
+cp -rpv  ${SOURCE_DIR}/zabora/zabora.conf.example   ${ZABBIX_DIR}/scripts/agentd/zabora/zabora.conf
+cp -rpv  ${SOURCE_DIR}/zabora/zabora.sh             ${ZABBIX_DIR}/scripts/agentd/zabora/
+cp -rpv  ${SOURCE_DIR}/zabora/zabbix_agentd.conf    ${ZABBIX_DIR}/zabbix_agentd.d/zabora.conf
+cp -rpv  ${SOURCE_DIR}/zabora/zabora.oraenv.example ${ZABBIX_DIR}/scripts/agentd/zabora/zabora.oraenv
 
 regex_array[0]="s|ORACLE_USER=.*|ORACLE_USER=\"${ORACLE_USER}\"|g"
 regex_array[1]="s|ORACLE_PASS=.*|ORACLE_PASS=\"${ORACLE_PASS}\"|g"
