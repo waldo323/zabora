c44563d60a221f3e7c93d1243a37bc1a2ce59a4a
djgreber <djgreber@gmail.com>
2019-04-05 15:10:21 +0200

    query modififed to output all wait classes

--- zabora/sql/db_waits.sql
+++ zabora/sql/db_waits.sql
@@ -4,10 +4,8 @@ SET      feedback OFF
 SET      verify OFF
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 SELECT
-    n.wait_class || ':' ||
-    TO_CHAR(m.time_waited/m.INTSIZE_CSEC,'FM99999999999999990.0000') retvalue
+    'wait_class_' || wait_class# || ':' ||
+    TO_CHAR(time_waited/INTSIZE_CSEC,'FM99999999999999990.0000') retvalue
 FROM
-    v$waitclassmetric m, v$system_wait_class n
-WHERE
-    m.wait_class_id=n.wait_class_id AND n.wait_class != 'Idle';
+    v$waitclassmetric;
 QUIT;
